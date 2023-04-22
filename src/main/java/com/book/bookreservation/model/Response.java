d8a11226767158059e72a23d42301949ed7f0b80
YuraLukianchenko <qlukianchenkop@gmail.com>
2023-04-22 12:14:20 +0300

    updates Response record

--- src/main/java/com/book/bookreservation/model/Response.java
+++ src/main/java/com/book/bookreservation/model/Response.java
@@ -6,4 +6,11 @@ import java.util.List;
 
 public record Response(@JsonInclude(JsonInclude.Include.NON_EMPTY) List<Book> books,
                        @JsonInclude(JsonInclude.Include.NON_EMPTY) List<String> errors) {
+
+    public static Response createResponseWithBooks(List<Book> books) {
+        return new Response(books, null);
+    }
+    public static Response createResponseWithErrors(List<String> errors) {
+        return new Response(null, errors);
+    }
 }
